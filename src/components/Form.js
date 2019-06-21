7eae90267cacf184e33ac1030622bb2d694cc57b
sajjal <nairsajjal@gmail.com>
2019-06-21 15:52:37 +0530

    Basic structure made

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+    render() {
+        return (
+            <form onSubmit={this.props.getWeather}>
+                <input type="text" name="city" placeholder="CITY"/>
+                <input type="text" name="country" placeholder="COUNTRY"/>
+               <button>Get Weather</button>
+            </form>
+        )
+    }
+}
+export default Form;
---
+++ src/components/Titles.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Titles extends React.Component{
+    render(){
+
+        return(
+
+            <div>
+
+                <h1>Weather Checker</h1>
+                <p>Get to know about temperature, conditions and more...</p>
+            </div>
+        )
+    }
+}
+
+export default Titles;
