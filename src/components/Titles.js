2634aaf6959e66a7f09cdebf65b34a80658b5b01
sajjal <nairsajjal@gmail.com>
2019-06-24 11:15:48 +0530

    Fixed Console Errors

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -3,7 +3,7 @@ import React from "react";
 const Titles = () => (
 	<div>
 		<h1 className="title-container__title">Weather Checker</h1>
-		<h3 className="title-container__subtitle"></h3>
+		
 	</div>
 );
 
